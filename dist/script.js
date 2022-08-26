b44f12405e937ed4d791374e259c559e44d5e112
Michael-Ber <mikeber000@gmail.com>
2022-08-26 13:14:35 +0800

    burger menu functional done, tabs on main done

--- dist/script.js
+++ dist/script.js
@@ -98,6 +98,80 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _phoneSpread__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./phoneSpread */ "./src/assets/js/phoneSpread.js");
 
 
+ //BURGER FUNCTION =============================================
+
+function burger() {
+  const burgerBtn = document.querySelector('.header__burger');
+  const burgerMenu = document.querySelector('.header__burger-menu');
+  const closeMenu = document.querySelectorAll('.close-nav');
+  const nextMenuBtns = document.querySelectorAll('.next');
+  const backBtn = document.querySelectorAll('.back-nav');
+  closeMenu.forEach(closeBtn => {
+    closeBtn.addEventListener('click', () => {
+      burgerMenu.classList.remove('header__burger-menu_active');
+    });
+  });
+  burgerBtn.addEventListener('click', () => {
+    burgerMenu.classList.toggle('header__burger-menu_active');
+  });
+  nextMenuBtns.forEach(nextBtn => {
+    nextBtn.addEventListener('click', e => {
+      e.preventDefault();
+      const btn = e.target;
+      btn.nextElementSibling.classList.add('burger-menu-header__nextlists_active');
+    });
+  });
+  backBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.parentNode.parentNode.classList.remove('burger-menu-header__nextlists_active');
+    });
+  });
+} //END BURGER FUNCTION ==========================================
+//TABS FUNCTION =================================================
+
+
+function tabs(tabsSelector, tabsContentSelector, tabsActive, contentActive, dataAttr) {
+  const tabsWrapper = document.querySelector(tabsSelector);
+  const contentWrapper = document.querySelector(tabsContentSelector);
+  const overlay = document.querySelector('.overlay');
+  const body = document.querySelector('body');
+
+  function resetTabs() {
+    Array.from(tabsWrapper.children).forEach(item => {
+      item.classList.remove(tabsActive);
+    });
+    Array.from(contentWrapper.children).forEach(item => {
+      item.classList.remove(contentActive);
+    });
+  }
+
+  function activateOverlay() {
+    overlay.classList.add('overlay_active');
+    overlay.style.height = `${body.scrollHeight}px`;
+  }
+
+  tabsWrapper.addEventListener('click', e => {
+    const target = e.target.hasAttribute(dataAttr) ? e.target : e.target.parentNode;
+    const targetAttr = target.getAttribute(dataAttr);
+    resetTabs();
+    activateOverlay();
+    target.classList.add(tabsActive);
+    Array.from(contentWrapper.children).forEach(item => {
+      if (item.getAttribute(dataAttr) === targetAttr) {
+        item.classList.add(contentActive);
+      }
+    });
+  });
+  window.addEventListener('click', e => {
+    console.log(e.target);
+
+    if (e.target.classList.contains('overlay_active')) {
+      e.target.classList.remove('overlay_active');
+      resetTabs();
+    }
+  });
+} //END TABS FUNCTION ==============================================
+
 
 window.addEventListener('DOMContentLoaded', () => {
   // image on main resize
@@ -129,11 +203,10 @@ window.addEventListener('DOMContentLoaded', () => {
   }); // END PHONE SPREAD ON HEADER
   //BURGER MENU
 
-  function burger() {
-    const burgerBtn = document.querySelector('.header__burger');
-    const nextMenuBtns = document.querySelectorAll('.item-nav_next');
-  } //END BURGER MENU
+  burger(); //END BURGER MENU
+  //MAIN TABS
 
+  tabs('.btns-tab-main__list', '.contents-tab-main__wrapper', 'btns-tab-main__item_active', 'contents-tab-main__content_active', 'data-tab'); //END MAIN TABS
 });
 
 /***/ }),
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,84 @@
 
 import {telSpread} from './phoneSpread';
 
+//BURGER FUNCTION =============================================
+function burger() {
+    const burgerBtn = document.querySelector('.header__burger');
+    const burgerMenu = document.querySelector('.header__burger-menu');
+    const closeMenu = document.querySelectorAll('.close-nav');
+    const nextMenuBtns = document.querySelectorAll('.next');
+    const backBtn = document.querySelectorAll('.back-nav');
+
+    closeMenu.forEach(closeBtn => {
+        closeBtn.addEventListener('click', () => {
+            burgerMenu.classList.remove('header__burger-menu_active');
+        });
+    });
+
+    burgerBtn.addEventListener('click', () => {
+        burgerMenu.classList.toggle('header__burger-menu_active');
+    });
+
+    nextMenuBtns.forEach(nextBtn => {
+        nextBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const btn = e.target;
+            btn.nextElementSibling.classList.add('burger-menu-header__nextlists_active');
+        });
+    });
+
+    backBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btn.parentNode.parentNode.classList.remove('burger-menu-header__nextlists_active');
+        });
+    })
+}
+
+//END BURGER FUNCTION ==========================================
+
+//TABS FUNCTION =================================================
+
+function tabs(tabsSelector, tabsContentSelector, tabsActive, contentActive, dataAttr) {
+    const tabsWrapper = document.querySelector(tabsSelector);
+    const contentWrapper = document.querySelector(tabsContentSelector);
+    const overlay = document.querySelector('.overlay');
+    const body = document.querySelector('body');
+    
+    function resetTabs() {
+        Array.from(tabsWrapper.children).forEach(item => {
+            item.classList.remove(tabsActive);
+        })
+        Array.from(contentWrapper.children).forEach(item => {
+            item.classList.remove(contentActive);
+        });
+    }
+    function activateOverlay() {
+        overlay.classList.add('overlay_active');
+        overlay.style.height = `${body.scrollHeight}px`; 
+    }
+    tabsWrapper.addEventListener('click', (e) => {
+        const target = e.target.hasAttribute(dataAttr) ? e.target : e.target.parentNode;
+        const targetAttr = target.getAttribute(dataAttr);
+        resetTabs();
+        activateOverlay();
+        target.classList.add(tabsActive);
+        Array.from(contentWrapper.children).forEach(item => {
+            if(item.getAttribute(dataAttr) === targetAttr) {
+                item.classList.add(contentActive);
+            }
+        });
+    });
+    window.addEventListener('click', (e) => {
+        console.log(e.target);
+        if(e.target.classList.contains('overlay_active')) {
+            e.target.classList.remove('overlay_active');
+            resetTabs();
+        }
+    });
+}
+
+//END TABS FUNCTION ==============================================
+
 window.addEventListener('DOMContentLoaded', () => {
 
     // image on main resize
@@ -39,12 +117,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //BURGER MENU
 
-    function burger() {
-        const burgerBtn = document.querySelector('.header__burger');
-        const nextMenuBtns = document.querySelectorAll('.item-nav_next');
-    }
-
+    
+    burger();
     //END BURGER MENU
+
+    //MAIN TABS
+    
+    tabs('.btns-tab-main__list', '.contents-tab-main__wrapper', 'btns-tab-main__item_active', 'contents-tab-main__content_active', 'data-tab');
+    
+    //END MAIN TABS
+
+
 
 
 });
